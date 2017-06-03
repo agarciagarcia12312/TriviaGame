14547721e6513d77becdad2ba0fb09b343dba519
andy garcia <andy garcia>
2017-06-03 11:26:41 -0600

    finished homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,155 @@
+var questions = ["The average person does what thirteen times a day?", "What is Johnny Depp afraid of?", "what country has the tallest building in the world" ,
+"In Kansas it's illegal to eat cherry pie with what?", "How tall am I?"];
+var q1 = ["bathroom", "laughs", "farts", "eats"];
+var q2 = ["spiders", "clowns", "flowers","elephants"];
+var q3 = ["U.A.E", "United States", "China", "Japan"];
+var q4 = ["Apples","Honey", "Milk", "Ice Cream"]
+var q5 = ["5'8", "6'4", "6'1", "5'3" ]
+var qs = [q1, q2, q3, q4, q5];
+var answers = [1,1,0,3,2]
+var guess = 0;
+var time = 15;
+// below q varible that tracks how many question have been created
+// var q = 0;
+
+
+$(document).ready(function() {
+// beow number of questions -1 
+var x = 4;	
+var correct = 0;
+var incorrect = 0;
+var interval1;
+
+
+	function displayed () {
+		x = questions.length -1;
+		var displayedOption = qs[x];
+		
+// below: code that displayes the question and choices
+		$("#question").html(questions[x]);		
+		$("#option1").html(displayedOption[0]);
+		$("#option2").html(displayedOption[1]);
+		$("#option3").html(displayedOption[2]);
+		$("#option4").html(displayedOption[3]);
+// below:code that deletes the last question 
+		questions.pop();	
+	}
+// below: function that hides all the the previos pages and dysplays new question
+	function newQ() {
+		$("#initial").hide();
+		$("#wrong").hide();
+		$("#correct").hide();
+		$("#master").show();
+		displayed();
+		timeReset();
+	}
+
+// below: function that displayes the final page with wins/ losses	
+	function final() {
+		$("#initial").hide();
+		$("#wrong").hide();
+		$("#correct").hide();
+		var percent = (correct / (correct + incorrect)) *100;
+		var data = "<p>Correct: " + correct +"</p>" +
+     		"<p>Incorrect: " + incorrect + "</P>"  +
+     		"<p>Percentage: " + percent + "%</P>";
+     	$("#data").html(data);	
+		$("#newGame").show();
+		$("#last").show();
+
+	}
+// on click function that starts the game
+	$("#new").click(newQ);
+
+// below: function checks if the question is answsered correctly and dysplayes correct page
+	function check () {
+		clearInterval(interval1);
+		
+			
+		if(((x == 0 ) && (guess == 1)) || ((x ==1 ) && (guess == 1)) || ((x == 2) && (guess == 0)) || ((x ==3 ) && (guess == 3)) || ((x ==4 ) && (guess == 2))) {
+			
+			correct++;
+			$("#master").hide();
+			$("#correct").show();
+			
+		}
+		else {
+			
+			incorrect++;
+			$("#master").hide();
+			$("#wrong").show();
+			
+		}
+		if (questions.length >0) {		
+			setTimeout(newQ, 2500);
+		}
+		else {
+			setTimeout(final, 4000)
+		}
+	}
+
+// clcik function for the user guess
+	$("#option1").click(function(){
+		guess = 0;
+		check();
+	})
+	$("#option2").click(function(){
+		guess = 1;
+		check();
+	})
+	$("#option3").click(function(){
+		guess = 2;
+		check();
+	})
+	$("#option4").click(function(){
+		guess = 3;
+		check();
+	})
+
+	
+
+	function timeReset () {
+		time = 15;
+		$("#time").html("00:15");
+		interval1 = setInterval(countdown, 1000); 
+	}
+
+	function countdown () {
+		time--;
+		// console.log(time);
+		var newTime = timeConvertor(time);
+		$("#time").html(newTime);
+// cheks if time has expired if it has it runs the chek function with a guess of 4 adding a loss
+		if (time == 0) {
+			guess = 5;
+			check();
+		}
+
+
+	}
+	function timeConvertor (t) {
+		var seconds = time;
+		if (time < 10) {
+			seconds = "0"+ time;
+		}
+		else {
+			seconds = time;
+		}
+		return "time left:  00:" + seconds;
+	}
+	
+	$("#newGame").click(function(){
+		console.log("new");
+		window.location.reload();
+	})
+
+	
+// 1.)create a timer that start with 30 seconds
+// 2.)take a second off every second and update time
+// 3.)when time equals 0 show the page for wrong answers and 
+// add one to wrong counter
+// if user clicks something cancel the timer 
+// 4.)everytime a new question is loaded set time to 30 sec
+// 5.) leave the displayed win/loose page for 5sec then go to the next question;
+
+})
